7e7d4f0b2c390e24b1ba25a6c6943d288558d5a6
Ionut Nohai <nohai.ionut02@gmail.com>
2025-08-27 12:19:04 +0300

    eslint update to disable prefer-const rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...{"prefer-const": ["warning"]}
 ];
 
 export default eslintConfig;
