f2a0b59e755d209addb9098fb2927c0451f790c9
Ionut Nohai <nohai.ionut02@gmail.com>
2025-09-18 16:15:21 +0300

    Add the domain for images for dev environment

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["rsefwg4r6i1uaxw3.public.blob.vercel-storage.com"]
-  }
+    domains: ["rsefwg4r6i1uaxw3.public.blob.vercel-storage.com", "sokdrkf3pdhcwr90.public.blob.vercel-storage.com"]
+  },
 };
 
 export default nextConfig;
